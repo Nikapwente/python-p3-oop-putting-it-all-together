ddcb9f2b03bc81fa86f16b859a1cfe5f2ef62860
Nikapwente <hrsmajor@gmail.com>
2023-12-12 20:45:51 +0300

    Assignment Complete

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,21 @@
 
 class Shoe:
     pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def get_shoe_size(self):
+            return self._size
+
+    def set_shoe_size(self, size=0):
+            if (type(size) == (int)):
+                self._size = size
+            else:
+                print("size must be an integer")
+
+    size = property(get_shoe_size, set_shoe_size,)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
